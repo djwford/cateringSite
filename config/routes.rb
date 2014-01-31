cbfae1525f93f32d0bdc11aa477538b0c862caa6
Derek <derekweatherford@gmail.com>
2014-01-31 10:09:50 -0700

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 CateringSite::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/menu"
+
   root :to => "home#index"
   devise_for :users
+  
 end
